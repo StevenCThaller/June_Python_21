ff4d1230cad77c7f674d0f6a5cbe5e475ec1c209
Cody Thaller <stevencthaller@gmail.com>
2021-07-14 13:28:56 -0700

    Whoops

--- W2/D3/01_Querying_SQL/test_script.sql
+++
@@ -1,6 +0,0 @@
-SELECT characters.character_name AS 'name', characters.alter_ego AS 'alter ego', leagues.league_name AS 'league name', alignments.alignment, powers.power_name AS 'power'
-FROM characters
-JOIN leagues ON characters.league_id = leagues.league_id
-JOIN alignments ON characters.alignment_id = alignments.alignment_id
-JOIN character_has_powers ON characters.character_id = character_has_powers.character_id
-JOIN powers ON character_has_powers.power_id = powers.power_id;
